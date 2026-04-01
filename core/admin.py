358a277a4485f656b7e4918c24fe563ce7bc0e47
Jenaro <jenaro@email.com>
2026-04-01 14:17:56 -0600

    base lista para activar multiempresa

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Turno, Incidencia
 from .models import Empresa
+from core.models import EmpresaUsuario
+
+admin.site.register(EmpresaUsuario)
 
 admin.site.register(Empresa)
 
