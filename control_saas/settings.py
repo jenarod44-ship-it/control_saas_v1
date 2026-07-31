3b2776aeadecdf7c90602523678f27c5ae663ecb
Jenaro <jenaro@email.com>
2026-07-30 18:48:14 -0600

    Corrige archivos estaticos en Render

--- control_saas/settings.py
+++ control_saas/settings.py
@@ -71,17 +71,16 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
 
     'core.middleware.EmpresaMiddleware',
-   
-    
 ]
 
 ROOT_URLCONF = 'control_saas.urls'
